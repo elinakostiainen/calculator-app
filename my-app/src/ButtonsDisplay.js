c3de4f53abf247d66467a1c7ae85f968ef3055ac
Elina Kostiainen <92241145+elinakostiainen@users.noreply.github.com>
2022-07-29 09:42:23 +1000

    Added the button display and screen for app

---
+++ my-app/src/ButtonsDisplay.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./ButtonsDisplay.css";
+
+export default function ButtonsDisplay(){
+    return (
+        <div className="ButtonsDisplay">
+                <button className="clear">AC</button>
+                <button className="delete">DEL</button>
+                <button className="divide">÷</button>
+                <button className="seven">7</button>
+                <button className="eight">8</button>
+                <button className="nine">9</button>
+                <button className="multiply">x</button>
+                <button className="four">4</button>
+                <button className="five">5</button>
+                <button className="six">6</button>
+                <button className="minus">-</button>
+                <button className="one">1</button>
+                <button className="two">2</button>
+                <button className="three">3</button>
+                <button className="add">+</button>
+                <button className="zero">0</button>
+                <button className="decimal">.</button>
+                <button className="equals">=</button>
+            </div>
+    )
+}
---
+++ my-app/src/Screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Screen.css";
+
+export default function Screen(){
+    return (
+        <div className="Screen">
+            <p className="input mt-2">12 x 6</p>
+            <p className="answer">72</p>
+        </div>
+    )
+}
